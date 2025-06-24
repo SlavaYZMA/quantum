eca06d892d88bb9b9b7f9258031f14ce23c810ea
SlavaYZMA <slavasolen@gmail.com>
2025-06-24 17:26:37 +0200

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,6 +1,5 @@
 const collapseSound = new Audio('https://example.com/collapse.wav'); // Замените на реальный путь к файлу
 
-window.playCollapseSound = function() {
-  collapseSound.currentTime = 0;
-  collapseSound.play().catch(e => console.log('Ошибка воспроизведения звука:', e));
-};
+function playSound(type) {
+  console.log(`Воспроизведение звука: ${type}`);
+}
