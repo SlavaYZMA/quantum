6f8dcc16beeed2d18931f6ed2ac0b1acd6005aee
SlavaYZMA <slavasolen@gmail.com>
2025-06-24 17:26:45 +0200

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,5 +1,3 @@
-const collapseSound = new Audio('https://example.com/collapse.wav'); // Замените на реальный путь к файлу
-
 function playSound(type) {
   console.log(`Воспроизведение звука: ${type}`);
 }
