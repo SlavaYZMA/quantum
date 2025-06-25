70e110c7d2a110e2e382f8f8b1dc9f04676b6cc3
SlavaYZMA <slavasolen@gmail.com>
2025-06-25 12:00:58 +0200

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,21 @@
-
+// Функция загрузки изображения
+window.loadImage = () => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = 'image/*';
+    input.onchange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (event) => {
+                const img = new Image();
+                img.src = event.target.result;
+                // Сохраняем изображение в глобальную переменную (пока не отображаем)
+                window.selectedImage = img;
+                console.log('Image loaded:', img.src);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+    input.click();
+};
