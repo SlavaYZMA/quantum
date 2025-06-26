e26ce0b49d65a3ca239707ba98d186200d8228f4
SlavaYZMA <slavasolen@gmail.com>
2025-06-26 17:24:46 +0200

    Update utils.js
    Обновлена функция loadImage для передачи изображения в анимацию.

--- js/utils.js
+++ js/utils.js
@@ -10,12 +10,21 @@ window.loadImage = () => {
             reader.onload = (event) => {
                 const img = new Image();
                 img.src = event.target.result;
-                // Сохраняем изображение в глобальную переменную (пока не отображаем)
-                window.selectedImage = img;
-                console.log('Image loaded:', img.src);
+                img.onload = () => {
+                    window.img = createImage(img.width, img.height); // Создаём p5.Image
+                    window.img.loadPixels();
+                    loadPixels(img, window.img);
+                    console.log('Image loaded and converted to p5.Image:', img.src);
+                };
             };
             reader.readAsDataURL(file);
         }
     };
     input.click();
 };
+
+// Вспомогательная функция для загрузки пикселей
+function loadPixels(imgElement, p5Image) {
+    p5Image.drawingContext.drawImage(imgElement, 0, 0);
+    p5Image.updatePixels();
+}
