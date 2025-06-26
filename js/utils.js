78cecd3fd03a690dd793ff6e4bc69a6df65fa1a7
SlavaYZMA <slavasolen@gmail.com>
2025-06-26 18:44:47 +0200

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,9 +1,11 @@
 // Функция загрузки изображения
 window.loadImage = () => {
+    console.log('loadImage function called');
     const input = document.createElement('input');
     input.type = 'file';
     input.accept = 'image/*';
     input.onchange = (e) => {
+        console.log('File input changed, processing file...');
         const file = e.target.files[0];
         if (file) {
             window.img = loadImage(URL.createObjectURL(file), () => {
@@ -14,7 +16,10 @@ window.loadImage = () => {
             }, (err) => {
                 console.error('Error loading image:', err);
             });
+        } else {
+            console.log('No file selected');
         }
     };
     input.click();
+    console.log('File input triggered');
 };
