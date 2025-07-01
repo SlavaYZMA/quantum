77fce85157e23469686654acb8e37222767abba0
SlavaYZMA <slavasolen@gmail.com>
2025-07-01 19:06:07 +0200

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,3 +1,5 @@
-function playSound(type) {
-  console.log(`Воспроизведение звука: ${type}`);
+// Простая реализация звука (заглушка, так как Pyodide не поддерживает полный аудио)
+function playQuantumSound() {
+    console.log('Playing quantum sound effect (placeholder)');
+    // В будущем можно добавить аудиофайл через p5.sound, если поддержка появится
 }
