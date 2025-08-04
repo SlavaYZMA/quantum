1cc841e26a83663396b14802b5b378d8dc9bca4c
SlavaYZMA <slavasolen@gmail.com>
2025-08-04 14:12:47 +0200

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -5,3 +5,14 @@ window.loadImage = () => {
         console.error('p5.js instance or loadImage not available');
     }
 };
+window.assembleText = function(button) {
+    button.style.color = '#00ffcc';
+    button.style.fontSize = '16px';
+    button.textContent = button.getAttribute('data-i18n') || button.id || button.textContent;
+};
+
+window.disassembleText = function(button) {
+    button.style.color = 'transparent';
+    button.style.fontSize = '0';
+    button.textContent = '';
+};
